b4892fc91072e9c660d21428317e65fa893f547c
Rudbeckia.hirta.L <83773602@qq.com>
2019-12-05 00:20:14 +0800

    Lua API 测试完毕

--- luago/state/lua_state.go
+++ luago/state/lua_state.go
@@ -4,6 +4,32 @@ type luaState struct {
 	stack *luaStack
 }
 
+
+/* TODO 补齐剩余方法 */
+func (self *luaState) IsBoolean(idx int) bool {
+	panic("implement me")
+}
+
+func (self *luaState) IsNumber(idx int) bool {
+	panic("implement me")
+}
+
+func (self *luaState) IsString(idx int) bool {
+	panic("implement me")
+}
+
+func (self *luaState) IsTable(idx int) bool {
+	panic("implement me")
+}
+
+func (self *luaState) IsThread(idx int) bool {
+	panic("implement me")
+}
+
+func (self *luaState) IsFunction(idx int) bool {
+	panic("implement me")
+}
+
 func New() *luaState {
 	return &luaState{stack:newLuaStack(20)}  // 栈的初始大小设为 20(拍脑袋
 }
